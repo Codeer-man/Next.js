cf5a0a416c91bd5460c9bbec4d20273a0a47a4e2
Codeer-man <mdrmoney34@gmail.com>
2024-09-19 16:53:50 +0545

    some small modification

--- e-commerce/src/app/components/catagory.tsx
+++ e-commerce/src/app/components/catagory.tsx
@@ -3,15 +3,15 @@
 import { useState } from 'react'
 
 export default function Catagory() {
-  
-  const [dropdown,setDropdown] = useState(false)
-    return (
+
+  const [dropdown, setDropdown] = useState(false)
+  return (
     <div>
-        <button
-            onClick={() => setDropdown((prev) => !prev)}
-        >
-            Catagory
-        </button>
+      <button
+        onClick={() => setDropdown((prev) => !prev)}
+      >
+        Catagory
+      </button>
     </div>
   )
 }
